c1c49d559987868b8d30a83461cf5bed58b68cd2
dadhy <evgenzolotoff@gmail.com>
2022-06-27 16:06:18 +0400

    add component

--- spacex/components/header.jsx
+++ spacex/components/header.jsx
@@ -5,17 +5,19 @@ import Navigation from './navigations/navigation'
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="headerContainer">
       <div className="content">
+        <div className="header">
         <Link href="/">
-          <a className="header__logo">
-            <div className="header__logo-corner">
-              <div className="header__logo-img" />
-            </div>
-          </a>
-        </Link>
-        <Navigation />
-        <MobileNavigation />
+            <a className="header__logo">
+              <div className="header__logo-corner">
+                <div className="header__logo-img" />
+              </div>
+            </a>
+          </Link>
+          <Navigation />
+          <MobileNavigation />
+        </div>
       </div>
     </div>
   )
---
+++ spacex/components/main.jsx
@@ -0,0 +1,49 @@
+import line from '../public/images/line.svg'
+
+const Main = () => {
+  return (
+    <div className="mainContent">
+      <div className="content">
+        <div className="flexContent">
+          <div className="tripContainer">
+            <div className="tripinfo">
+              <span className="trip">Путешествие</span>
+            </div>
+            <div className="tripinfo">
+              <span className="planet">на красную планету</span>
+            </div>
+            <div className="tripinfo">
+              <p className="start">Начать путешествие</p>
+              <img className="line" src={line.src}></img>
+            </div>
+          </div>
+          <div className="goodList">
+            <div className="item1">
+              <p>Мы</p>
+              <p className="bigtext">1</p>
+              <p>На рынке</p>
+            </div>
+            <div className="item2">
+              <p>Гарантируем</p>
+              <p className="bigtext">50%</p>
+              <p>Безопасности</p>
+            </div>
+            <div className="item3">
+              <p>Календарик за </p>
+              <p className="bigtext">2001<span>г.</span></p>
+              <p>в подарок</p>
+            </div>
+            <div className="item4">
+              <p>Путешествие</p>
+              <p className="bigtext">597</p>
+              <p>Дней</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="moon"></div>
+    </div>
+ )
+}
+
+export default Main
